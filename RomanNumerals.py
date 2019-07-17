604b9e7dd212938ce793840bfd0e158e97777762
Nicholas Maltbie <nick.dmalt@gmail.com>
2019-07-17 15:19:10 -0700

    Creating Initial Challenge
    Created initial Roman Numeral challenge

---
+++ RomanNumerals.py
@@ -0,0 +1,25 @@
+"""
+Roman Numerals
+
+Convert roman numerals to decimal and back
+
+
+"""
+
+def toRoman(decimalNumber: str) -> str:
+    """ Converts decimal numbers (as string data type) to roman numerals (as string as well)
+    >>> toRoman("10"):
+    "X"
+    >>> toRoman("4"):
+    "IV"
+    """
+    pass
+
+def toDecimal(romanNumber: str) -> str:
+    """ Converts roman numbers (as string data type) to decimal numerals (as string as well)
+    >>> toDecimal("X"):
+    "10"
+    >>> toDecimal("IV"):
+    "4"
+    """
+    pass
